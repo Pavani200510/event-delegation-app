4bedb2ef24135314c9d67a4a1fb73483d1da7bad
Pavani 200510 <pavaninagaraju487@gmail.com>
2026-05-17 12:10:58 +0530

    add useEffect hook example

--- src/App.js
+++ src/App.js
@@ -1,22 +1,2 @@
-import EventDelegation from  "./components/EventDelegation";
-import BuildInfo from "./components/buildInfo";
-import UseStateExample from "./components/UseStateExample";
-import UseEffectExample from "./components/UseEffectExample";
-import UseRefExample from "./components/UseRefExample";
-import FetchData from "./components/FetchData";
-import Counter from "./components/counter/counter";
-function App(){
-  return(
-    <div>
-      <EventDelegation/>
-      <BuildInfo/>
-      <UseStateExample/>
-      <UseEffectExample/>
-      <UseRefExample/>
-      <FetchData/>
-      <Counter/>
 
-    </div>
-  );
-}
-  export default App;
+      

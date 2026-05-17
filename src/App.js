ba4d2d9b89264ba558ad76b2c9a020aa2f404fd7
Pavani 200510 <pavaninagaraju487@gmail.com>
2026-05-17 13:07:50 +0530

    add components to src

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 
 import UseEffectExample from "./components/UseEffectExample";
-import { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState('');
-
-  useEffect(() => {
-    console.log("Count changed:", count);
-    document.title = `Count: ${count}`;
-  }, [count]);
-
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h2>useState Hook</h2>
-      <h1>Count: {count}</h1>
-
-      <button onClick={() => setCount(count + 1)}>+</button>
-      <button onClick={() => setCount(count - 1)}>-</button>
-
-      <h2 style={{ marginTop: '30px' }}>useEffect Hook</h2>
-      <input
-        type="text"
-        placeholder="Type your name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <p>Hello {name}</p>
+    <div>
+      <UseEffectExample />
     </div>
   );
 }

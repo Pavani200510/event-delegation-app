2dc117f117fa0db60bbb082f88f48b42a38d17b1
Pavani 200510 <pavaninagaraju487@gmail.com>
2026-05-17 12:46:16 +0530

    Add FecthData component

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -6,7 +6,7 @@ function FetchData() {
 
   useEffect(() => {
 
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch("https:/jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((data) => setUsers(data));
 
